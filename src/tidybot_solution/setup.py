0f52cc725da06a63b6c8e14ac811a7ff770f43e9
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-04-15 14:32:32 +0100

    Made node added params

---
+++ src/tidybot_solution/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tidybot_solution'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='villanelle',
+    maintainer_email='46252182+nonbinary-duck@users.noreply.github.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'identify_cubes = tidybot_solution.identify_cubes:main'
+        ],
+    },
+)
