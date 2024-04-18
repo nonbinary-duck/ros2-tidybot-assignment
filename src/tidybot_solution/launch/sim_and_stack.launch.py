df540a224efc0776b4090c7d8f0d0bef3fc35d60
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-04-18 12:55:32 +0100

    Isn't quite pushing right

--- src/tidybot_solution/launch/sim_and_stack.launch.py
+++ src/tidybot_solution/launch/sim_and_stack.launch.py
@@ -5,11 +5,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
-
+from launch.conditions import IfCondition
 #
 # Launch file based on the substitutions tutorial: https://docs.ros.org/en/humble/Tutorials/Intermediate/Launch/Using-Substitutions.html
 #
@@ -18,9 +18,29 @@ def generate_launch_description():
 
     # Line of code adapted from the nav2_bringup slam_launch.py file
     tidybot_dir = get_package_share_directory('tidybot_solution');
+
+    # Allow user to decide map
+    world_name_arg = DeclareLaunchArgument(
+        "world",
+        default_value="level_1_1.world",
+        description="Name of the world file in uol_tidybot package"
+    );
+    world_name = LaunchConfiguration("world");
+    
+    # Allow user to decide if there are red and green cubes or just green
+    add_red_arg = DeclareLaunchArgument(
+        "add_red_cubes",
+        default_value="False",
+        description="If red cubes should be used as well as green"
+    );
+    add_red = LaunchConfiguration("add_red_cubes");
     
     # Initialise a launch description object to add to
     ld = LaunchDescription();
+
+    # Add our arguments
+    ld.add_action(world_name_arg);
+    ld.add_action(add_red_arg);
 
     ld.add_action(
         # Include the tidybot launch description
@@ -34,7 +54,8 @@ def generate_launch_description():
             ]),
             launch_arguments={
                 "use_rviz": "false",
-                "params_file": os.path.join( tidybot_dir, "params", "limo_params.yaml" )
+                "params_file": os.path.join( tidybot_dir, "params", "limo_params.yaml" ),
+                "world": world_name
             }.items()
         )
     );
@@ -107,7 +128,11 @@ def generate_launch_description():
                 "red:=true",
                 "-p",
                 "m_objects:=10"
-            ]
+            ],
+            # Only add them if we want them
+            condition=IfCondition(
+                PythonExpression([ add_red ])
+            )
         )
     );
 
